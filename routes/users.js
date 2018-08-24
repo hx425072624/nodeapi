4895cd3b38d72cddcd8310a0ac256a3f3ec7a9c8
hx <hx425072624@gmail.com>
2018-08-24 16:00:15 +0800

    init

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models/user');
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+router.get('/setup', function(req, res, next) {
+
+  // 创建一个测试用户
+  var nick = new User({
+    name: 'waitifsh',
+    password: 'test',
+    admin: true
+  });
+
+  // 将测试用户保存到数据库
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+module.exports = router;
